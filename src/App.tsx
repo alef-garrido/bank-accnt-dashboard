9783dae65d8d9e34caa5bb460331bdd344de9858
a_lemat <OA.GARRIDO11@GMAIL.COM>
2026-01-01 20:30:22 -0600

    chore: initial project setup and remove boilerplate
    Add dev install and configs; remove default boilerplate files and assets

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { BalanceChart } from './components/BalanceChart'
+
+function App() {
+
+  return (
+   
+      <div className='w-[80vw] h-auto'>
+        <BalanceChart />
+      </div>
+    
+  )
+}
+
+export default App
